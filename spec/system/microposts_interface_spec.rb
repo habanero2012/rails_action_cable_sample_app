22b3cc67ecc84af2b0605f4fa7b6d680126fbc54
habanero2012 <sato1101@live.jp>
2020-06-02 02:21:31 +0900

    homeページのデザインを修正

--- spec/system/microposts_interface_spec.rb
+++ spec/system/microposts_interface_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'micropost interface', test: :system do
     visit root_path
 
     # 無効な送信
-    click_button 'Post'
+    click_button 'ツイートする'
     expect(page).to have_content "Content can't be blank"
 
     # 有効な送信
@@ -18,7 +18,7 @@ RSpec.describe 'micropost interface', test: :system do
     expect {
       fill_in 'micropost_content', with: content
       page.find_by_id('micropost_picture').set("#{Rails.root}/spec/files/rails.png")
-      click_button 'Post'
+      click_button 'ツイートする'
       expect(page).to have_current_path root_path
       expect(page).to have_content content
     }.to change(Micropost, :count).by(1)
@@ -37,13 +37,13 @@ RSpec.describe 'micropost interface', test: :system do
     login_as(user)
     visit root_path
 
-    expect(page).to have_content '1 micropost'
+    expect(page).to have_content 'Microposts1'
 
     # micropostが0個のユーザー
     other_user = create(:user)
     login_as(other_user)
     visit root_path
 
-    expect(page).to have_content '0 microposts'
+    expect(page).to have_content 'Microposts0'
   end
 end
